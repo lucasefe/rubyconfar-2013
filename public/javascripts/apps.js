86a6e760f5340bf4ffdce464209c9ed3bfe30219
Nico Sztabzyb <nico@szstudios.net>
2013-07-14 19:37:55 -0300

    Adding 4 Speakers.

--- public/javascripts/apps.js
+++ public/javascripts/apps.js
@@ -1,4 +1,10 @@
 // Placeholders for IE
 $(document).ready(function(){
   Placeholders.init();
+
+
+  $("#speakers ul li a").hover(function() {
+    $(this).parents('li').find("img").toggleClass('highlight');    
+  });
+  
 });
